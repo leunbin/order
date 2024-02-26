1a147a1d98d5b011c5b116d4447262ea29f710c6
doah kim <rayla3546@gmail.com>
2024-02-26 18:44:53 +0900

    prettier format

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -7,15 +7,22 @@ const authRouter = express.Router();
 //POST / api /v1 /auth/sign-up
 authRouter.post(
   "/sign-up",
-  //미들웨어 생기면 예시postMiddleware.checkCompletePostFrom("body"), 
+  //미들웨어 생기면 예시postMiddleware.checkCompletePostFrom("body"),
   authController.postSignUp
 );
 
 //POST / api /v1 /auth/sign-in
 authRouter.post(
-    "/sign-in",
-    //postMiddleware.checkCompletePostFrom("body"), 
-    authController.postSignIn 
-  );
-  
+  "/sign-in",
+  //postMiddleware.checkCompletePostFrom("body"),
+  authController.postSignIn
+);
+
+//POST / api /v1 /auth/withdraw
+authRouter.post(
+  "/withdraw",
+  //postMiddleware.checkCompletePostFrom("body"),
+  authController.postWithdraw
+);
+
 module.exports = authRouter;
