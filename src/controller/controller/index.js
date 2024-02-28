3ebe32746b69e437450bb8f9c6bb0d9280290ed7
[코치] 한인호 <oneitwoh@gmail.com>
2024-02-28 10:48:01 +0900

    대문자 소문자 파일 이름 이슈 해결1

--- src/controller/controller/index.js
+++
@@ -1,9 +0,0 @@
-const postController = require("./postController");
-const categoryController = require("../../../src/controller/categoryController");
-const productController = require("../../../src/controller/productController");
-
-module.exports = {
-  postController,
-  categoryController,
-  productController,
-};
